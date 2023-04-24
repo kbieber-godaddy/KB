4940a3354e0ce916df1bcf17426a59ad209db879
kbieber-godaddy <97202437+kbieber-godaddy@users.noreply.github.com>
2023-04-23 21:34:58 -0700

    update

--- pytrain.py
+++ pytrain.py
@@ -1,10 +1,33 @@
-print("Hello World!")
-print("Hello Again")
-print ("I like typing this")
-print ("This is fun")
-print ("I'd much rather you 'not'.")
-print ('I "said" do not touch this')
-       
+# print("Hello World!")
+# print("Hello Again")
+# print ("I like typing this")
+# print ("This is fun")
+# print ("I'd much rather you 'not'.")
+# print ('I "said" do not touch this')
+#print ("Here's another (fancy) line")      
 ##
+
+# Adding sectiion
+print ("I will now countmy chickens:")
+print ("Hens", 25 + 30 / 6)
+print ("Roosters", 100 - 25 * 3 % 4)
+print ("Now i will count the eggs:")
+
+print (3 + 2 + 1 -5 +4 % 2 - 1 / 4 + 6)
+print ("Is it true that 3 + 2 < 5 -7?")
+print (3 + 2 < 5 -7)
+print ("What is 3 + 2?", 3+2)
+
+print ("What is 5 -7?", 5 -7)
+print ("Oh, that's why it's False.")
+
+print ("How about some more.")
+print ("Is it greater?", 5 > -2)
+print ("Is it greater or equal?", 5 >= -2)
+print ("Is it less or equal?", 5 <= 2)
+
+
+print("is it false that 7 * 5 > 6 * 2")
+print (7*5 > 6*2)
 
 
